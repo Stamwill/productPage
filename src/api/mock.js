6d4fb83f5a52032f169ca8ddfec5c87e8cfb3cd6
William <williamstam@gmail.com>
2022-06-05 16:55:31 +0100

    [mock.js] Add data

---
+++ src/api/mock.js
@@ -0,0 +1,20 @@
+export const logo = ['/images/logo.svg']
+export const hamburger = ['/images/iconMenu.svg']
+
+export const navList = [
+  {
+    heading: 'Collections',
+  },
+  {
+    heading: 'Men',
+  },
+  {
+    heading: 'Women',
+  },
+  {
+    heading: 'About',
+  },
+  {
+    heading: 'Contact',
+  },
+]
