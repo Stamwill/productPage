719131c28a56fcb6fa0ffabde3c31a339f614d68
William <williamstam@gmail.com>
2022-06-05 12:40:28 +0100

    [index] Add font

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
@@ -8,11 +8,17 @@ export default function Home() {
         <title>Product Page</title>
         <meta name="description" content="Ecommerce product page" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
 
       <main className={styles.main}>
         <h1>Hello</h1>
       </main>
     </div>
-  );
+  )
 }
