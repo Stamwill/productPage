aa07bc8c4ab5df237234dd8b716a1117c2a5f3cd
William <williamstam@gmail.com>
2022-06-05 12:31:14 +0100

    [first commit]

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Product Page</title>
+        <meta name="description" content="Ecommerce product page" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1>Hello</h1>
+      </main>
+    </div>
+  );
+}
