81309e048f7ffe90fa7ab476e361d5257c9416a3
William <williamstam@gmail.com>
2022-06-06 11:05:03 +0100

    [mock.js] Prepare data for future use

--- src/api/mock.js
+++ src/api/mock.js
@@ -1,5 +1,7 @@
 export const logo = ['/images/logo.svg']
 export const hamburger = ['/images/iconMenu.svg']
+export const shoppingCartLogo = ['/images/iconCart.svg']
+export const userAvatar = ['/images/imageAvatar.png']
 
 export const navList = [
   {
@@ -18,3 +20,32 @@ export const navList = [
     heading: 'Contact',
   },
 ]
+
+export const product = [
+  {
+    img: './images/imageProduct1.jpg',
+    thumb: '/images/imageProduct1Thumbnail.jpg',
+  },
+  {
+    img: './images/imageProduct2.jpg',
+    thumb: '/images/imageProduct2Thumbnail.jpg',
+  },
+  {
+    img: './images/imageProduct3.jpg',
+    thumb: '/images/imageProduct3Thumbnail.jpg',
+  },
+  {
+    img: './images/imageProduct4.jpg',
+    thumb: '/images/imageProduct4Thumbnail.jpg',
+  },
+]
+
+export const productInfo = {
+  heading: 'Sneaker Company',
+  subHeading: 'Fall Limited Edition Sneakers',
+  text: "These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.",
+}
+
+export const productPrice = {
+  price: '$125.00',
+}
