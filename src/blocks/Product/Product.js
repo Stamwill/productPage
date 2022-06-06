1c92247c1414864cdb6648cd50e4907f69cd291e
William <williamstam@gmail.com>
2022-06-06 15:02:12 +0100

    [Product] Import slider and product description

--- src/blocks/Product/Product.js
+++ src/blocks/Product/Product.js
@@ -1,13 +1,21 @@
 import * as React from 'react'
+import Slider from 'components/Slider'
 import classes from './Product.module.css'
+import ProductDescription from 'blocks/ProductDescription'
 
 const Product = (props) => {
   const { product, productInfo, productPrice } = props
   return (
     <section className={classes.root}>
       <div className={classes.imgContainer}>
-        <img className={classes.productImg} src={product[0].img} alt="test" />
+        <Slider>
+          {product.map((item, idx) => (
+            <img className={classes.productImg} src={item.img} alt="test" key={idx} />
+          ))}
+        </Slider>
       </div>
+
+      <ProductDescription productInfo={productInfo} />
     </section>
   )
 }
