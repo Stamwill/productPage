4fda2902e102581334a14096217312336f5277fe
William <williamstam@gmail.com>
2022-06-06 11:06:15 +0100

    [index.js] Move font links

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,12 +9,6 @@ export default function Home() {
         <title>Product Page</title>
         <meta name="description" content="Ecommerce product page" />
         <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
       </Head>
 
       <App />
