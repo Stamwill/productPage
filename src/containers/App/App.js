d7d0bc4a168000ff46dd75ec00531ca0a74f1729
William <williamstam@gmail.com>
2022-06-07 13:52:34 +0100

    [App] Add state and click handlers

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -5,15 +5,32 @@ import Product from 'blocks/Product'
 import classes from './App.module.css'
 
 const App = () => {
+  const [addedItems, setAddedItems] = React.useState(0)
+
+  const handleAdd = () => {
+    console.log('add')
+    setAddedItems((addedItems += 1))
+  }
+  const handleSubtract = () => {
+    console.log('subtract')
+    setAddedItems(addedItems + -1)
+  }
+
   return (
     <div className={classes.root}>
       <header>
         <AppNav navList={navList} />
       </header>
       <main className={classes.main}>
-        <Product product={product} productInfo={productInfo} productPrice={productPrice} />
+        <Product
+          product={product}
+          productInfo={productInfo}
+          productPrice={productPrice}
+          handleAdd={handleAdd}
+          handleSubtract={handleSubtract}
+          addedItems={addedItems}
+        />
       </main>
-      <footer>Footer</footer>
     </div>
   )
 }
