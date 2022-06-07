f53929ff0b4bea1d079ad44c6732b897ebea8421
William <williamstam@gmail.com>
2022-06-07 13:51:28 +0100

    [Product] Import blocks

--- src/blocks/Product/Product.js
+++ src/blocks/Product/Product.js
@@ -1,10 +1,14 @@
 import * as React from 'react'
 import Slider from 'components/Slider'
-import classes from './Product.module.css'
 import ProductDescription from 'blocks/ProductDescription'
+import ProductPrice from 'blocks/ProductPrice'
+import AddProduct from 'blocks/AddProduct'
+import AddCart from 'blocks/AddCart'
+import PropTypes from 'prop-types'
+import classes from './Product.module.css'
 
 const Product = (props) => {
-  const { product, productInfo, productPrice } = props
+  const { product, productInfo, productPrice, handleAdd, handleSubtract, addedItems } = props
   return (
     <section className={classes.root}>
       <div className={classes.imgContainer}>
@@ -16,8 +20,22 @@ const Product = (props) => {
       </div>
 
       <ProductDescription productInfo={productInfo} />
+
+      <div className={classes.addCartContainer}>
+        <ProductPrice productPrice={productPrice} />
+        <AddProduct handleAdd={handleAdd} handleSubtract={handleSubtract} addedItems={addedItems} />
+        <AddCart />
+      </div>
     </section>
   )
+}
+
+Product.propTypes = {
+  product: PropTypes.array,
+  productInfo: PropTypes.object,
+  productPrice: PropTypes.array,
+  handleAdd: PropTypes.func,
+  addedItems: PropTypes.number,
 }
 
 export default Product
