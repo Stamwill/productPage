f420e3930e9912c479990424a39fb89907ceaffb
William <williamstam@gmail.com>
2022-06-08 09:13:14 +0100

    [Hamburger] Add props to Hamburger

--- src/components/Hamburger/Hamburger.js
+++ src/components/Hamburger/Hamburger.js
@@ -1,13 +1,27 @@
 import * as React from 'react'
 import { hamburger } from 'api/mock'
+import classnames from 'clsx'
+import propTypes from 'prop-types'
 import classes from './Hamburger.module.css'
 
-const Hamburger = (props) => {
+const Hamburger = React.forwardRef(function Hamburger(props, ref) {
+  const { className, toggleMenu, open } = props
+
   return (
-    <div className={classes.hamburgerMenu}>
+    <div
+      className={classnames(classes.hamburgerMenu, { [classes.menuOpen]: open }, className)}
+      ref={ref}
+      onClick={toggleMenu}
+    >
       <img src={hamburger} alt="hamburger menu" />
     </div>
   )
+})
+
+Hamburger.propTypes = {
+  className: propTypes.string,
+  toggleMenu: propTypes.func,
+  open: propTypes.bool,
 }
 
 export default Hamburger
