5cddc37f8854b2d73d01d29ff5d3c6f623f909b7
William <williamstam@gmail.com>
2022-06-09 09:42:43 +0100

    [Hamburger] Fix propTypes and add media query

--- src/components/Hamburger/Hamburger.js
+++ src/components/Hamburger/Hamburger.js
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { hamburger } from 'api/mock'
 import classnames from 'clsx'
-import propTypes from 'prop-types'
+import PropTypes from 'prop-types'
 import classes from './Hamburger.module.css'
 
 const Hamburger = React.forwardRef(function Hamburger(props, ref) {
@@ -19,9 +19,9 @@ const Hamburger = React.forwardRef(function Hamburger(props, ref) {
 })
 
 Hamburger.propTypes = {
-  className: propTypes.string,
-  toggleMenu: propTypes.func,
-  open: propTypes.bool,
+  className: PropTypes.string,
+  toggleMenu: PropTypes.func,
+  open: PropTypes.bool,
 }
 
 export default Hamburger
