0efa8cfe312ca822659a54553b298e9b0f6fcfee
William <williamstam@gmail.com>
2022-06-10 10:46:15 +0100

    [AddCart] Add props and hoverEffect

--- src/blocks/AddCart/AddCart.js
+++ src/blocks/AddCart/AddCart.js
@@ -3,13 +3,18 @@ import { shoppingCartLogo } from 'api/mock'
 import PropTypes from 'prop-types'
 import classes from './AddCart.module.css'
 
-const AddCart = () => {
+const AddCart = (props) => {
+  const { handleShoppingCart } = props
   return (
-    <div className={classes.root}>
+    <div className={classes.root} onClick={handleShoppingCart}>
       <img src={shoppingCartLogo} className={classes.cartImg} alt="add to cart" />
       <span>Add to cart</span>
     </div>
   )
+}
+
+AddCart.propTypes = {
+  handleShoppingCart: PropTypes.func,
 }
 
 export default AddCart
