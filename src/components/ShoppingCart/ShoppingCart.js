30a724ce48868a8da226be616283cd456c9f3633
William <williamstam@gmail.com>
2022-06-13 22:31:09 +0100

    [ShoppingCart] Add props

--- src/components/ShoppingCart/ShoppingCart.js
+++ src/components/ShoppingCart/ShoppingCart.js
@@ -4,9 +4,9 @@ import PropTypes from 'prop-types'
 import classes from './ShoppingCart.module.css'
 
 const ShoppingCart = (props) => {
-  const { handleShoppingCart, basketItems } = props
+  const { handleShoppingCart, basketItems, toggleBasket } = props
   return (
-    <div className={classes.root} onClick={handleShoppingCart}>
+    <div className={classes.root} onClick={toggleBasket}>
       <img src={shoppingCartLogo} className={classes.cartImg} alt="Shopping Cart" />
       {basketItems ? <div className={classes.basketCounter}>{basketItems}</div> : null}
     </div>
