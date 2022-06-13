622b600bcf60be339bbb634131528661bf750f01
William <williamstam@gmail.com>
2022-06-13 22:32:05 +0100

    [AppNav] Add Basket and props

--- src/containers/App/partials/AppNav.js
+++ src/containers/App/partials/AppNav.js
@@ -1,13 +1,14 @@
 import * as React from 'react'
 import Hamburger from 'components/Hamburger'
 import ShoppingCart from 'components/ShoppingCart'
+import Basket from 'blocks/Basket'
 import { logo, userAvatar } from 'api/mock'
 import PropTypes from 'prop-types'
 import classnames from 'clsx'
 import classes from './AppNav.module.css'
 
 const AppNav = (props) => {
-  const { navList, toggleMenu, open, basketItems } = props
+  const { navList, toggleMenu, open, basketItems, toggleBasket, basketIsOpen } = props
   return (
     <section className={classes.root}>
       <Hamburger toggleMenu={toggleMenu} open={open} />
@@ -25,12 +26,12 @@ const AppNav = (props) => {
 
       <div className={classes.userBox}>
         <div className={classes.cartContainer}>
-          <ShoppingCart basketItems={basketItems} />
-          {/* <img
-            className={classnames(classes.img, classes.shoppingCart)}
-            src={shoppingCartLogo}
-            alt="shopping cart"
-          /> */}
+          <ShoppingCart basketItems={basketItems} toggleBasket={toggleBasket} />
+          <Basket
+            toggleBasket={toggleBasket}
+            basketIsOpen={basketIsOpen}
+            basketItems={basketItems}
+          />
         </div>
         <img
           className={classnames(classes.img, classes.userAvatar)}
