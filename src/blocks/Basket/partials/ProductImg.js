7e0f5ec37293ed496de34bf2626dbcef57012870
William <williamstam@gmail.com>
2022-06-15 11:04:14 +0100

    [ProductImg] Add PropTypes

--- src/blocks/Basket/partials/ProductImg.js
+++ src/blocks/Basket/partials/ProductImg.js
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import PropTypes from 'prop-types'
 import classes from './ProductImg.module.css'
 
 const ProductImg = (props) => {
@@ -8,6 +9,10 @@ const ProductImg = (props) => {
       <img className={classes.img} src={product[0].thumb} alt="product" />
     </div>
   )
+}
+
+ProductImg.propTypes = {
+  product: PropTypes.array,
 }
 
 export default ProductImg
