5e535c548ec6be4a785e1486bd8e3145f10764db
William <williamstam@gmail.com>
2022-06-15 11:04:43 +0100

    [Basket] Add spacing, pass prop to checkout

--- src/blocks/Basket/Basket.js
+++ src/blocks/Basket/Basket.js
@@ -37,7 +37,7 @@ const Basket = (props) => {
         ) : (
           <p className={classes.emptyBasket}>Basket is empty</p>
         )}
-        <Checkout />
+        <Checkout toggleBasket={toggleBasket} />
       </div>
     </section>
   )
