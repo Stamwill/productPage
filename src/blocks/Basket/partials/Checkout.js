ec2e4b185fbd478f44f896d0d86fe3c1c91f2da2
William <williamstam@gmail.com>
2022-06-15 11:03:46 +0100

    [Checkout] Add PropType and clickEvent

--- src/blocks/Basket/partials/Checkout.js
+++ src/blocks/Basket/partials/Checkout.js
@@ -1,12 +1,20 @@
 import * as React from 'react'
+import PropTypes from 'prop-types'
 import classes from './Checkout.module.css'
 
-const Checkout = () => {
+const Checkout = (props) => {
+  const { toggleBasket } = props
   return (
     <div className={classes.checkout}>
-      <button className={classes.btn}>Checkout</button>
+      <button className={classes.btn} onClick={toggleBasket}>
+        Checkout
+      </button>
     </div>
   )
+}
+
+Checkout.propTypes = {
+  toggleBasket: PropTypes.func,
 }
 
 export default Checkout
