9c23572acc145ff86e6adfdd390379669c86c094
William <williamstam@gmail.com>
2022-06-15 09:33:43 +0100

    [DeleteProduct] Add props and PropTypes

--- src/blocks/Basket/partials/DeleteProduct.js
+++ src/blocks/Basket/partials/DeleteProduct.js
@@ -1,13 +1,19 @@
 import * as React from 'react'
 import { iconDelete } from 'api/mock'
+import PropTypes from 'prop-types'
 import classes from './DeleteProduct.module.css'
 
-const DeleteProduct = () => {
+const DeleteProduct = (props) => {
+  const { handleDelete } = props
   return (
     <div>
-      <img src={iconDelete} alt="delete" />
+      <img className={classes.bin} src={iconDelete} alt="delete" onClick={handleDelete} />
     </div>
   )
+}
+
+DeleteProduct.propTypes = {
+  handleDelete: PropTypes.func,
 }
 
 export default DeleteProduct
